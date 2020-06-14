1efcde4611da9de4de03465be1feccaf52edaa85
Jonah Duckles <jonah@duckles.org>
2020-06-14 13:32:52 +1200

    split template out to own file

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 import subprocess
 from app import app
@@ -20,9 +20,11 @@ def pandoc(arguments, cwd=None):
     print("Running {}".format(command))
     subprocess.run(command, cwd=cwd)
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -45,37 +47,10 @@ def upload_file():
             pandoc([full_filename,'--reference-doc=/app/templates/line-numbers.docx', '-o', full_filename], cwd=UPLOAD_FOLDER)
             return redirect(url_for('uploaded_file',
                                     filename=filename))
-    return '''
-    <!doctype html>
-    <head><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"></head>
-    <title>Add Linenumbers to Word Documents</title>
-    <div class="container">
-    <div class="jumpotron">
-    <h1 class="display-4">Upload a File</h1>
-    <p class="lead">Upload a Word Document and it will be converted to have line-numbers and be sent back to you</p>
-    <form method=post enctype=multipart/form-data>
-    <div class="form-group">
-    <div class="custom-file">
-        <input type=file  class="custom-file-input" name=file id="customFile">
-  <label class="custom-file-label" for="customFile">Choose file</label>
-    </div>
-    </div>
-    <div class="form-group">
-    <input type=submit class="btn btn-primary" value=Upload>
-    </div>
-    </div>
-    </form>
-    </div>
-    </div>
- <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-
-<script type="application/javascript">
-    $('input[type="file"]').change(function(e){
-        var fileName = e.target.files[0].name;
-        $('.custom-file-label').html(fileName);
-    });
-</script>
-'''
+    return render_template("upload_form.html",
+            title="Add Line Numbers",
+            help_text="Upload a Word Document and it will be converted to have line-numbers and be sent back to you"]
+            )
 
 
 @app.route('/uploads/<filename>')
